57c61a001703d29b1fe80bf0487e24a79db910a4
Milan Panik <milan.panik1@gmail.com>
2019-11-21 15:10:19 +0100

    Update protos after IC upgrade

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -4,9 +4,7 @@ set -e
 set -x
 
 COMMON_PROTO_FILE=../shared/protos/proto/common/v1/common.proto
-SRC_DAM_PROTO_FILE=../shared/protos/dam_service.proto
 SRC_IC_PROTO_FILE=../shared/protos/ic_service.proto
-OUT_DAM_FILE=dam-service
 OUT_IC_FILE=ic-service
 TMP_DIR="./tmp"
 TMP_PROTO_FILE=${TMP_DIR}/temporary.proto
@@ -19,12 +17,6 @@ rm -rf ${OUT_DIR}
 
 mkdir -p ${TMP_DIR}
 mkdir -p ${OUT_DIR}
-
-# DAM
-cp ${SRC_DAM_PROTO_FILE} ${TMP_PROTO_FILE}
-
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_DAM_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
-${PBTS_BIN} -o ${OUT_DIR}/${OUT_DAM_FILE}.d.ts ${OUT_DIR}/${OUT_DAM_FILE}.js
 
 # IC
 cp ${SRC_IC_PROTO_FILE} ${TMP_PROTO_FILE}
