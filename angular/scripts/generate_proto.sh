712ed8274c281f7bae3f639ee95ab02955530163
Milan Panik <milan.panik1@gmail.com>
2019-12-04 12:08:03 +0100

    DISCO-2557 Add proto file for users

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -6,8 +6,10 @@ set -x
 COMMON_PROTO_FILE=../shared/protos/proto/common/v1/common.proto
 SRC_IC_PROTO_FILE=../shared/protos/ic_service.proto
 OUT_IC_FILE=ic-service
-SRC_TOKEN_PROTO_FILE=../shared/protos/token_service.proto
-OUT_TOKEN_FILE=token-service
+SRC_TOKENS_PROTO_FILE=../shared/protos/tokens.proto
+OUT_TOKENS_FILE=token-service
+SRC_USERS_PROTO_FILE=../shared/protos/users.proto
+OUT_USERS_FILE=user-service
 TMP_DIR="./tmp"
 TMP_PROTO_FILE=${TMP_DIR}/temporary.proto
 OUT_DIR="./src/app/shared/proto"
@@ -26,8 +28,14 @@ cp ${SRC_IC_PROTO_FILE} ${TMP_PROTO_FILE}
 ${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_IC_FILE}.d.ts ${OUT_DIR}/${OUT_IC_FILE}.js
 
-# TOKEN
-cp ${SRC_TOKEN_PROTO_FILE} ${TMP_PROTO_FILE}
+# TOKENS
+cp ${SRC_TOKENS_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_TOKEN_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
-${PBTS_BIN} -o ${OUT_DIR}/${OUT_TOKEN_FILE}.d.ts ${OUT_DIR}/${OUT_TOKEN_FILE}.js
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_TOKENS_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
+${PBTS_BIN} -o ${OUT_DIR}/${OUT_TOKENS_FILE}.d.ts ${OUT_DIR}/${OUT_TOKENS_FILE}.js
+
+# USERS
+cp ${SRC_USERS_PROTO_FILE} ${TMP_PROTO_FILE}
+
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_USERS_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE}
+${PBTS_BIN} -o ${OUT_DIR}/${OUT_USERS_FILE}.d.ts ${OUT_DIR}/${OUT_USERS_FILE}.js
