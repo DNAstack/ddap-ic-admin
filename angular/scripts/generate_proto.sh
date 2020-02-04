ff688df3c109cea623f97d7026c445893186b787
Milan Panik <milan.panik1@gmail.com>
2020-02-04 08:57:06 +0100

    Use symlink for common.proto

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -3,18 +3,21 @@
 set -e
 set -x
 
-COMMON_PROTO_FILE=../shared/protos/proto/common/v1/common.proto
-COMMON_OAUTHCLIENT_PROTO_FILE=../shared/protos/proto/common/v1/oauthclient.proto
-COMMON_ACCOUNT_PROTO_FILE=../shared/protos/proto/common/v1/account.proto
+SRC_COMMON_PROTO_FILE=../shared/protos/proto/common/v1/common.proto
+SRC_COMMON_OAUTHCLIENT_PROTO_FILE=../shared/protos/proto/common/v1/oauthclient.proto
+SRC_COMMON_ACCOUNT_PROTO_FILE=../shared/protos/proto/common/v1/account.proto
 SRC_IC_PROTO_FILE=../shared/protos/ic_service.proto
-OUT_IC_FILE=ic-service
 SRC_TOKENS_PROTO_FILE=../shared/protos/token_service.proto
-OUT_TOKENS_FILE=token-service
 SRC_USERS_PROTO_FILE=../shared/protos/user_service.proto
+
+OUT_IC_FILE=ic-service
+OUT_TOKENS_FILE=token-service
 OUT_USERS_FILE=user-service
+
 TMP_DIR="./tmp"
 TMP_PROTO_FILE=${TMP_DIR}/temporary.proto
 OUT_DIR="./src/app/shared/proto"
+
 PBJS_BIN="./node_modules/protobufjs/bin/pbjs"
 PBTS_BIN="./node_modules/protobufjs/bin/pbts"
 
@@ -27,17 +30,17 @@ mkdir -p ${OUT_DIR}
 # IC
 cp ${SRC_IC_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE} ${COMMON_OAUTHCLIENT_PROTO_FILE} ${COMMON_ACCOUNT_PROTO_FILE}
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE} ${SRC_COMMON_PROTO_FILE} ${SRC_COMMON_OAUTHCLIENT_PROTO_FILE} ${SRC_COMMON_ACCOUNT_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_IC_FILE}.d.ts ${OUT_DIR}/${OUT_IC_FILE}.js
 
 # TOKENS
 cp ${SRC_TOKENS_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_TOKENS_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE} ${COMMON_OAUTHCLIENT_PROTO_FILE} ${COMMON_ACCOUNT_PROTO_FILE}
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_TOKENS_FILE}.js ${TMP_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_TOKENS_FILE}.d.ts ${OUT_DIR}/${OUT_TOKENS_FILE}.js
 
 # USERS
 cp ${SRC_USERS_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_USERS_FILE}.js ${TMP_PROTO_FILE} ${COMMON_PROTO_FILE} ${COMMON_OAUTHCLIENT_PROTO_FILE} ${COMMON_ACCOUNT_PROTO_FILE}
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_USERS_FILE}.js ${TMP_PROTO_FILE}
 ${PBTS_BIN} -o ${OUT_DIR}/${OUT_USERS_FILE}.d.ts ${OUT_DIR}/${OUT_USERS_FILE}.js
