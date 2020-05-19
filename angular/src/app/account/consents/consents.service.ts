9bac85d9462746e375dc6f28b239e0138ed54c50
Milan Panik <milan.panik1@gmail.com>
2020-05-19 12:13:07 +0200

    DISCO-2847 Add missing realm to request

--- angular/src/app/account/consents/consents.service.ts
+++ angular/src/app/account/consents/consents.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import ListConsentsResponse = consents.v1.ListConsentsResponse;
+import { realmIdPlaceholder } from 'ddap-common-lib';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 import { consents } from '../../shared/proto/ic-service';
-import ListConsentsResponse = consents.v1.ListConsentsResponse;
 
 @Injectable({
   providedIn: 'root',
@@ -16,14 +17,16 @@ export class ConsentsService {
 
   getConsents(userId: string, params = {}): Observable<ListConsentsResponse> {
     return this.http.get<ListConsentsResponse>(
-      `${environment.idpBaseUrl}/identity/v1alpha/users/${encodeURIComponent(userId)}/consents`,
+      `${environment.idpBaseUrl}/identity/v1alpha/${realmIdPlaceholder}`
+      + `/users/${encodeURIComponent(userId)}/consents`,
       { params }
     );
   }
 
   revokeConsent(userId: string, consentId: string): Observable<null> {
     return this.http.delete<any>(
-      `${environment.idpBaseUrl}/identity/v1alpha/users/${encodeURIComponent(userId)}/consents/${encodeURIComponent(consentId)}`
+      `${environment.idpBaseUrl}/identity/v1alpha/${realmIdPlaceholder}`
+      + `/users/${encodeURIComponent(userId)}/consents/${encodeURIComponent(consentId)}`
     );
   }
 
