01cc3f3c2cbd8dd5d7affe3ed12634d944ef8f42
Milan Panik <milan.panik1@gmail.com>
2020-05-19 14:08:28 +0200

    DISCO-2847 Skip consents e2e tests if skip variable is set

--- e2e-tests/src/main/java/com/dnastack/ddap/frontend/AdminConsentsE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/frontend/AdminConsentsE2eTest.java
@@ -2,6 +2,8 @@ package com.dnastack.ddap.frontend;
 
 import com.dnastack.ddap.common.page.AdminListPage;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.dnastack.ddap.common.fragments.NavBar.consentsLink;
@@ -10,12 +12,20 @@ import static com.dnastack.ddap.common.fragments.NavBar.consentsLink;
 @SuppressWarnings("Duplicates")
 public class AdminConsentsE2eTest extends AbstractAdminFrontendE2eTest {
 
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Assume.assumeFalse(
+            "IC consent is disabled, skipping consents' e2e tests",
+            Boolean.parseBoolean(optionalEnv("E2E_WALLET_SKIP_IC_CONSENT","false"))
+        );
+    }
+
     @Test
     public void shouldListConsents() {
         AdminListPage adminListPage = ddapPage.getNavBar()
                 .goTo(consentsLink(), AdminListPage::new);
 
-        adminListPage.assertListItemExists("consents/fake-consent");
+        adminListPage.assertTableNotEmpty();
     }
 
 }
