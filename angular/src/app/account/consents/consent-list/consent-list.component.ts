ca7a422994afb31431f060e33995547fa52fc9f6
Milan Panik <milan.panik1@gmail.com>
2020-05-25 12:01:12 +0200

    DISCO-2847 Update consents page

--- angular/src/app/account/consents/consent-list/consent-list.component.ts
+++ angular/src/app/account/consents/consent-list/consent-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { flatMap, switchMap } from 'rxjs/operators';
 
@@ -14,7 +16,8 @@ import ListConsentsResponse = consents.v1.ListConsentsResponse;
 })
 export class ConsentListComponent implements OnInit {
 
-  readonly displayedColumns: string[] = ['name', 'user', 'client', 'items', 'scopes', 'resources', 'moreActions'];
+  readonly displayedColumns: string[] = ['name', 'client', 'scopes', 'visas', 'createdAt', 'expiresAt', 'moreActions'];
+  readonly dayjs: Function = dayjs;
 
   consents$: Observable<ListConsentsResponse>;
 
@@ -24,6 +27,7 @@ export class ConsentListComponent implements OnInit {
     private consentsService: ConsentsService,
     private userService: UserService
   ) {
+    dayjs.extend(utc);
   }
 
   ngOnInit() {
@@ -43,6 +47,15 @@ export class ConsentListComponent implements OnInit {
         flatMap((user) => this.consentsService.revokeConsent(user.id, consentId))
       )
       .subscribe(() => this.refreshConsents$.next(undefined));
+  }
+
+  // Example of 'name' 'users/ic_ba090f2bb80c42bc9e73263b145/consents/770c138d-c18d-43eb-b826-624196de6acf' where last
+  // part is 'id'
+  getIdFromName(name: string): string {
+    if (!name || !name.includes('/')) {
+      return name;
+    }
+    return name.substring(name.lastIndexOf('/') + 1);
   }
 
 }
