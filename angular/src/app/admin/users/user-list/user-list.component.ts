a8073505f6f40e6f6b5446351701a06a42791d6e
Milan Panik <milan.panik1@gmail.com>
2020-07-21 10:21:18 +0200

    DISCO-2949 Add paginator back to users list

--- angular/src/app/admin/users/user-list/user-list.component.ts
+++ angular/src/app/admin/users/user-list/user-list.component.ts
@@ -4,8 +4,7 @@ import IUser = scim.v2.IUser;
 import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute, Router } from '@angular/router';
-import { realmIdPlaceholder } from 'ddap-common-lib';
-import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import IListUsersResponse = scim.v2.IListUsersResponse;
 import { flatMap, switchMap } from 'rxjs/operators';
 
@@ -34,7 +33,7 @@ export class UserListComponent implements OnInit {
   users$: Observable<IListUsersResponse>;
   realm: string;
 
-  private readonly defaultPageSize = 25;
+  private readonly defaultPageSize = 10;
   private readonly refreshUsers$ = new BehaviorSubject<any>({ startIndex: 1, count: this.defaultPageSize });
 
   constructor(private userService: UserService,
@@ -102,7 +101,7 @@ export class UserListComponent implements OnInit {
                 return this.identityService.invalidateTokens();
               } else {
                 this.refreshUsers$.next({});
-                return EMPTY;
+                return of();
               }
             })
           )
