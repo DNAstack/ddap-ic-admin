b1fcec34c15430fe11ed5dc0a7fd9331743bbe82
Milan Panik <milan.panik1@gmail.com>
2020-07-29 11:57:08 +0200

    DISCO-3031 Show dialog when secret is generated

--- angular/src/app/admin/identity-concentrator/clients/client-manage/client-manage.component.ts
+++ angular/src/app/admin/identity-concentrator/clients/client-manage/client-manage.component.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormValidationService } from 'ddap-common-lib';
-import { ConfigModificationModel, EntityModel } from 'ddap-common-lib';
+import { ConfigModificationModel, EntityModel, SecretGeneratedDialogComponent } from 'ddap-common-lib';
 
 import { IcConfigEntityFormComponentBase } from '../../shared/ic/ic-config-entity-form-component.base';
 import { ClientFormComponent } from '../client-form/client-form.component';
@@ -18,10 +19,13 @@ export class ClientManageComponent extends IcConfigEntityFormComponentBase {
   @ViewChild(ClientFormComponent)
   clientForm: ClientFormComponent;
 
-  constructor(protected route: ActivatedRoute,
-              protected router: Router,
-              protected validationService: FormValidationService,
-              private clientService: ClientService) {
+  constructor(
+    protected route: ActivatedRoute,
+    protected router: Router,
+    protected validationService: FormValidationService,
+    private dialog: MatDialog,
+    private clientService: ClientService
+  ) {
     super(route, router, validationService);
   }
 
@@ -33,7 +37,32 @@ export class ClientManageComponent extends IcConfigEntityFormComponentBase {
     const client: EntityModel = this.clientForm.getModel();
     const change = new ConfigModificationModel(client.dto, {});
     this.clientService.save(client.name, change)
-      .subscribe(() => this.navigateUp('../..'), this.showError);
+      .subscribe(
+        (response) => {
+          const secret = response.client_secret;
+          if (secret) {
+            this.openSecretGeneratedDialog(secret);
+          } else {
+            this.navigateUp('../..');
+          }
+        },
+        this.showError
+      );
+  }
+
+  openSecretGeneratedDialog(secretValue: string) {
+    this.dialog.open(SecretGeneratedDialogComponent, {
+      disableClose: true, // prevent closing dialog by clicking on backdrop
+      data: {
+        secretLabel: 'client secret',
+        secretValue,
+      },
+    }).afterClosed()
+      .subscribe(({ acknowledged }) => {
+        if (acknowledged) {
+          this.navigateUp('../..');
+        }
+      });
   }
 
 }
