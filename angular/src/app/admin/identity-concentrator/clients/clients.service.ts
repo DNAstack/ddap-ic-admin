fa57ee72b9a54ce759b19dd17955e4f9babc30b3
Monica <monica@dnastack.com>
2020-07-29 12:00:25 -0400

    DISCO-3028 make client application config edit a PATCH request

--- angular/src/app/admin/identity-concentrator/clients/clients.service.ts
+++ angular/src/app/admin/identity-concentrator/clients/clients.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ErrorHandlerService } from 'ddap-common-lib';
+import { ConfigModificationModel, ErrorHandlerService, realmIdPlaceholder } from 'ddap-common-lib';
+import { Observable } from 'rxjs';
 
+import { environment } from '../../../../environments/environment';
 import { IcConfigEntityType } from '../shared/ic/ic-config-entity-type.enum';
 import { IcConfigService } from '../shared/ic/ic-config.service';
 
@@ -17,4 +19,11 @@ export class ClientService extends IcConfigService {
     super(IcConfigEntityType.clients, http, errorHandler);
   }
 
+  update(id: string, change: ConfigModificationModel): Observable<any> {
+    return this.http.patch(`${environment.idpApiUrl}/${realmIdPlaceholder}/config/${this.entityType}/${id}`,
+      change
+    ).pipe(
+      this.errorHandler.notifyOnError(`Can't update ${id}.`)
+    );
+  }
 }
