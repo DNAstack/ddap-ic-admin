3c7088c6352737d952687f81fb428c12daae3e38
Milan Panik <milan.panik1@gmail.com>
2020-08-14 12:10:28 +0200

    [#174335120] Fix Options E2E tests after IC upgrade

--- e2e-tests/src/main/java/com/dnastack/ddap/frontend/AdminIcOptionsE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/frontend/AdminIcOptionsE2eTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static com.dnastack.ddap.common.fragments.NavBar.icOptionsLink;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
 @SuppressWarnings("Duplicates")
@@ -23,25 +22,14 @@ public class AdminIcOptionsE2eTest extends AbstractAdminFrontendE2eTest {
     }
 
     @Test
-    public void submitNumberOptionWithoutTypeError() {
-        AdminOptionPage adminListPage = ddapPage.getNavBar()
-                                                .goToAdminOptionPage(icOptionsLink());
-
-        assertThat(adminListPage.getOptionNames(), hasItem("Account Name Length"));
-        final String oldValue = adminListPage.getOptionValue("Account Name Length");
-        adminListPage.submitOption("Account Name Length", "accountNameLength", oldValue);
-        adminListPage.assertNoError("Account Name Length", 5);
-    }
-
-    @Test
     public void submitStringOptionWithoutTypeError() {
         AdminOptionPage adminListPage = ddapPage.getNavBar()
                                                 .goToAdminOptionPage(icOptionsLink());
 
-        assertThat(adminListPage.getOptionNames(), hasItem("Default Passport Token TTL"));
-        final String oldValue = adminListPage.getOptionValue("Default Passport Token TTL");
-        adminListPage.submitOption("Default Passport Token TTL", "defaultPassportTokenTtl", oldValue);
-        adminListPage.assertNoError("Default Passport Token TTL", 5);
+        assertThat(adminListPage.getOptionNames(), hasItem("Claim TTL Cap"));
+        final String oldValue = adminListPage.getOptionValue("Claim TTL Cap");
+        adminListPage.submitOption("Claim TTL Cap", "claimTtlCap", oldValue);
+        adminListPage.assertNoError("Claim TTL Cap", 5);
     }
 
 }
